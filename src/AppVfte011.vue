bcf11e30a065303f37e09e48cc6de2e8f58679d3
tassun <32536248+tassun@users.noreply.github.com>
2024-10-28 10:42:19 +0700

    initial project

---
+++ src/AppVfte011.vue
@@ -0,0 +1,85 @@
+<!-- App.vue -->
+<template>
+  <div id="fswaitlayer" class="fa fa-spinner fa-spin"></div>
+  <div class="pt-page pt-page-current pt-page-controller search-pager">
+    <PageHeader ref="pageHeader" :labels="labels" pid="vfte011" version="1.0.0" showLanguage="true" @language-changed="changeLanguage" :multiLanguages="multiLanguages" :build="buildVersion" />
+    <SearchForm ref="searchForm" :labels="labels" :dataCategory="dataCategory" @data-select="dataSelected" @data-insert="dataInsert" />
+  </div>
+  <teleport to="#modaldialog">
+    <EntryForm ref="entryForm" :labels="labels" :dataCategory="dataCategory" @data-saved="dataSaved" @data-updated="dataUpdated" @data-deleted="dataDeleted" />
+  </teleport>
+</template>
+<script>
+import { ref } from 'vue';
+import { PageHeader } from '@willsofts/will-control';
+import SearchForm from '@/components/SearchForm.vue';
+import EntryForm from '@/components/EntryForm.vue';
+import { getLabelModel, getMultiLanguagesModel } from "@willsofts/will-app";
+import { getDefaultLanguage, setDefaultLanguage } from "@willsofts/will-app";
+import { startApplication } from "@willsofts/will-app";
+
+const buildVersion = process.env.VUE_APP_BUILD_DATETIME;
+export default {
+  components: {
+    PageHeader, SearchForm, EntryForm
+  },
+  setup() {
+    let labels = ref(getLabelModel());
+    const multiLanguages = ref(getMultiLanguagesModel());
+    return { buildVersion, multiLanguages, labels };
+  },
+  mounted() {
+    console.log("App: mounted ...");
+    this.$nextTick(async () => {
+      //ensure ui completed then invoke startApplication 
+      startApplication("vfte011",(data) => {
+        this.multiLanguages = getMultiLanguagesModel();
+        this.messagingHandler(data);
+        this.$refs.pageHeader.changeLanguage(getDefaultLanguage());
+      });
+    });
+  },
+  methods: {
+    messagingHandler(data) {
+      console.log("messagingHandler: data",data); 
+    },
+    changeLanguage(lang) {
+      setDefaultLanguage(lang);
+      let labelModel = getLabelModel(lang);
+      this.labels = labelModel;
+    },
+    dataSelected(item,action) {
+      //listen action from search form
+      console.log("App: dataSelected",item,"action",action);
+      if("edit"==action) {
+        this.$refs.entryForm.retrieveRecord({reservenum: item.reservenum});
+      } else if("delete"==action) {
+        this.$refs.entryForm.startDeleteRecord({reservenum: item.reservenum});
+      }
+    },
+    dataInsert(filters) {
+      //listen action from search form
+      console.log("App: record insert",filters);
+      this.$refs.entryForm.startInsertRecord();
+    },
+    dataSaved(data,response) {
+      //listen action from entry form when after saved
+      console.log("App: record saved");
+      console.log("data",data,"response",response);
+      this.$refs.searchForm.search();
+    },
+    dataUpdated(data,response) {
+      //listen action from entry form when after updated
+      console.log("App: record updated");
+      console.log("data",data,"response",response);
+      this.$refs.searchForm.search();
+    },
+    dataDeleted(data,response) {
+      //listen action from entry form when after deleted
+      console.log("App: record deleted");
+      console.log("data",data,"response",response);
+      this.$refs.searchForm.search(true);
+    },
+  }
+};
+</script>
